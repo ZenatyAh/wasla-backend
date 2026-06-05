a0774ac99b6a5f55b569b935505f82bb04e25ba2
Ahmedzenaty <zenatyah@gmail.com>
2026-06-05 23:57:48 +0300

    fix: update server listen address to allow external connections
    Changed the server listen address from localhost to '0.0.0.0' to enable access from external clients.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import app from "./server.js";
 
 const port = Number(process.env.PORT || 3000);
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server running on http://localhost:${port}`);
 });
